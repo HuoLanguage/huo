9a51b191f0a026cbb5343bd4139b6eb7b41bdd64
2^400 <thelonewolfling@gmail.com>
2016-07-04 12:08:16 -0700

    Change map to have optional index

--- src/execution_functions/map_array.c
+++ src/execution_functions/map_array.c
@@ -8,8 +8,16 @@ struct Value map_array(struct Tree * ast, hash_table *defined, struct Scopes * s
     if (max_depth <= 0) {
         ERROR("Max depth exceeded in computation");
     }
-    if (ast->size < 4) {
-        ERROR("Not enough arguments for map_array: %i < 4\n", ast->size);
+    bool use_index;
+    int func_index;
+    if (ast->size == 4) {
+        use_index = true;
+        func_index = 3;
+    } else if (ast->size == 3) {
+        use_index = false;
+        func_index = 2;
+    } else {
+        ERROR("Wrong number of arguments for map__array: %i != [3,4]\n", ast->size);
     }
     struct Value array = execute(ast->children[0], defined, scopes, max_depth - 1);
     if (array.type != ARRAY) {
@@ -17,10 +25,12 @@ struct Value map_array(struct Tree * ast, hash_table *defined, struct Scopes * s
     }
     for(int i = 0; i < array.data.array->size; i++){
         struct Value *item = value_copy_heap(array.data.array->values[i]);
-        struct Value index = value_from_long(i);
-        struct Tree * function = duplicate_tree(ast->children[3]);
+        struct Tree * function = duplicate_tree(ast->children[func_index]);
         store_let_value(&ast->children[1]->content, item, scopes);
-        store_let_value(&ast->children[2]->content, &index, scopes);
+        if (use_index) {
+            struct Value index = value_from_long(i);
+            store_let_value(&ast->children[2]->content, &index, scopes);
+        }
         struct Value result = execute(function, defined, scopes, max_depth - 1);
         array.data.array->values[i] = value_copy_heap(&result);
     }
