9a2ac33dbe8afc16e979d5ce352e0e899b66a159
2^400 <thelonewolfling@gmail.com>
2016-07-04 11:17:38 -0700

    use value_from_long in map_array

--- src/execution_functions/map_array.c
+++ src/execution_functions/map_array.c
@@ -17,12 +17,7 @@ struct Value map_array(struct Tree * ast, hash_table *defined, struct Scopes * s
     }
     for(int i = 0; i < array.data.array->size; i++){
         struct Value *item = value_copy_heap(array.data.array->values[i]);
-        struct Value index = {
-            .type = LONG,
-            .data = {
-                .ln=(long)i
-            }
-        };
+        struct Value index = value_from_long(i);
         struct Tree * function = duplicate_tree(ast->children[3]);
         store_let_value(&ast->children[1]->content, item, scopes);
         store_let_value(&ast->children[2]->content, &index, scopes);
